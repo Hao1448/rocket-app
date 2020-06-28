16a70eda463616f4774d0889bcdea4af1fed050f
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-29 02:03:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled, { ThemeProvider} from 'styled-components'
-import { GlobalTheme } from './base/theme'
-import { GlobalStyle } from './base/styles'
-import { WidgetPosts, Header } from './components'
+import { GlobalTheme } from 'base/theme'
+import { GlobalStyle } from 'base/styles'
+import { WidgetPosts, Header } from 'components'
 
 function App() {
   return (
