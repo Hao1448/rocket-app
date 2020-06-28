61f4466de4fa5ebc6abad53330499847e34bf992
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-29 02:04:39 +0300

    update Container

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,10 +1,13 @@
 import React  from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components'
 
 const Container = ({ children }) => {
     return <Wrapper>{children}</Wrapper>
 }
-
+Container.propTypes = {
+    children: PropTypes.node
+}
 const Wrapper = styled.div`
     max-width: 1182px;
     margin: 0 auto;
