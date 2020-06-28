2ec2c631df4d55eed3a72fdd4ab5d36e5fc0f421
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-29 02:04:03 +0300

    Update theme.js

--- src/base/theme.js
+++ src/base/theme.js
@@ -10,14 +10,7 @@ export const GlobalTheme = {
     },
     color: {
         primary: 'rgb(255, 105, 0)',
-        primary_dark: 'rgb(217, 99, 52)',
-        secondary: '#FF0066',
         black: '#333333',
-        white: '#FFFFFF',
-        grey: '#F3F3F3',
-        error: '#D30000',
-        grey_light: '#F0F0F0',
-        grey_dark: '#D5D5D5',
-        grey_deepdark: '#ADADAD',
+        grey: '#ADADAD',
     }
 }
